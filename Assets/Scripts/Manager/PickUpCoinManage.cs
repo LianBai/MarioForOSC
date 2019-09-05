4a3990728c938c5db5cef5ecee6132b84918de1b
zongquanli <841774407@qq.com>
2019-09-05 08:46:03 +0800

    局内动态加载金币02

--- Assets/Scripts/Manager/PickUpCoinManage.cs
+++ Assets/Scripts/Manager/PickUpCoinManage.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
 using QF;
+using QF.Action;
 using UniRx;
 using QF.Res;
 using QF.Extensions;
@@ -12,13 +13,14 @@ namespace QFramework.MFO
     public class PickUpCoinManage : MonoSingleton<PickUpCoinManage>
     {
         public ResLoader mResLoader;
-        
+        public Sequence mSequence;
         public List<Transform> mAllocTransforms;
         public Queue<Vector3> mCoinPosList;
 
         public override void OnSingletonInit()
         {
             mResLoader = ResLoader.Allocate();
+            mSequence = DOTween.Sequence();
             mAllocTransforms = new List<Transform>();
             mCoinPosList = new Queue<Vector3>();
 
@@ -37,8 +39,19 @@ namespace QFramework.MFO
             var jsonText = ResLoadManage.Instance.mResLoader
                 .LoadSync<TextAsset>("pickup_game_" + level.ToString()).text;
             mCoinPosList = jsonText.FromJson<Queue<Vector3>>();
-            mAllocTransforms.ForEach(coinTf => StartCoroutine(AddCoin(coinTf)));
+            //利用dotween形成间隔加载金币从而做成不同步动画
+            int temp = 0;
+            mAllocTransforms.ForEach(coinTf => mSequence.Append(DOTween.To(
+                () => temp, x => temp = x, temp, 0.02f
+                )
+                .OnComplete(() =>
+                {
+                    AddCoin(coinTf);
+                })
+                ));
+            //mAllocTransforms.ForEach(coinTf => AddCoin(coinTf));
         }
+
         /// <summary>
         /// 当金币被吃掉后金币进行回收
         /// </summary>
@@ -47,7 +60,11 @@ namespace QFramework.MFO
         {
             if (mCoinPosList.Count != 0)
             {
-                StartCoroutine(AddCoin(tF));
+                //StartCoroutine(AddCoin(tF));
+            }
+            else
+            {
+                tF.gameObject.SetActive(false);
             }
         }
         /// <summary>
@@ -55,13 +72,17 @@ namespace QFramework.MFO
         /// </summary>
         /// <param name="coinTf"></param>
         /// <returns></returns>
-        public IEnumerator AddCoin(Transform coinTf)
+        public void AddCoin(Transform coinTf)
         {
-            yield return new WaitForSeconds(0.1f);
             coinTf.ApplySelfTo(self =>
             {
+                //var coinClip = self.GetComponent<Animation>().clip;
+                //AnimationEvent coinPlayEvent = new AnimationEvent();
+                //coinPlayEvent.time = 1.0f * Random.Range(0, 10) / 10f;
                 self.localPosition = mCoinPosList.Dequeue();
                 self.gameObject.SetActive(true);
+                //coinClip.AddEvent(coinPlayEvent);
+                //self.gameObject.GetComponent<Animation>().clip = 0.1f*Random.Range(0,10);
             });
         }
     }
